e892b8762f4de37c7a89a80444b07c235480a2b4
Diego Alvis <diegoalvispal@gmail.com>
2020-04-20 17:16:07 -0500

    Add dotenv lib and create development environment file

--- data/lib/remote_datasource/api/store_api.dart
+++ data/lib/remote_datasource/api/store_api.dart
@@ -1,6 +1,5 @@
 import 'package:data/remote_datasource/parse/api_service_impl.dart';
 import 'package:data/remote_datasource/parse/api_service.dart';
-import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 /// Class to handle all related to the Store API request using the Parse SDK Manager
 /// Reference https://dashboard.back4app.com/apidocs/5IgxoMa1l6QoEJodtOXcQmXCmZF61S4yGbhhXLE5#Store-custom-class
