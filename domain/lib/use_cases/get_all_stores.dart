d7cd700240db0d8b39496b7d734e825f3e6202df
Diego Alvis <diegoalvispal@gmail.com>
2020-04-22 17:59:31 -0500

    Add porgress at store list page

--- domain/lib/use_cases/get_all_stores.dart
+++ domain/lib/use_cases/get_all_stores.dart
@@ -1,12 +1,23 @@
 import 'package:domain/entities/store_entity.dart';
 import 'package:domain/repositories/store_repository.dart';
 
+// TODO remove this
 class GetAllStores {
   final StoreRepository _repository;
 
   GetAllStores(this._repository);
 
   Future<List<StoreEntity>> call(String city) async {
-    return await _repository.getAllStores(city);
+    return _repository.getAllStores(city);
+  }
+}
+
+class GetAllStoresByCity {
+  final StoreRepository _repository;
+
+  GetAllStoresByCity(this._repository);
+
+  Future<List<StoreEntity>> call(String city) async {
+    return _repository.getAllStores(city);
   }
 }
