b65cc8b83288a4ffc94d3c774505f1e9513e0651
Diego Alvis <diegoalvispal@gmail.com>
2020-10-12 10:36:13 -0500

    update some packages

--- lib/presentation/adresses/my_address_page.dart
+++ lib/presentation/adresses/my_address_page.dart
@@ -99,7 +99,7 @@ class _MyAddressPageState extends State<MyAddressPage> {
                                     builder: (BuildContext context) {
                                       return AlertDialog(
                                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                                        title: Text("Elige tu ciudad", style: AppTextStyle.section),
+                                        title: Text(strings.selectYourCity, style: AppTextStyle.section),
                                         content: citiesDialog(),
                                       );
                                     });
